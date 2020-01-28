3bc5f454debd11d55387c3aaeae7525bfa3e5405
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-01-28 21:43:04 +0100

    Tests + all functions related on Pixels

--- src/image.rs
+++ src/image.rs
@@ -1,8 +1,51 @@
+#[derive(Copy, Clone)]
 pub struct Pixel {
-    pub r: u8,
-    pub g: u8,
-    pub b: u8
+    r: u8,
+    g: u8,
+    b: u8
 }
+impl Pixel{
+    pub fn new(red: u8, green: u8, blue: u8) -> Pixel{
+        Pixel{r: red, g: green, b: blue}
+    }
+
+    pub fn red(self) -> u8 {
+        self.r
+    }
+
+    pub fn blue(self) -> u8 {
+        self.b
+    }
+
+    pub fn green(self) -> u8 {
+        self.g
+    }
+
+    pub fn display(self) -> String {
+        let mut result = String::new();
+        result.push_str(&self.red().to_string());
+        result.push(',');
+        result.push_str(&self.green().to_string());
+        result.push(',');
+        result.push_str(&self.blue().to_string());
+        return result;
+    }
+
+    pub fn inverse(&mut self){
+        self.r = 255 - self.r;
+        self.g = 255 - self.g;
+        self.b = 255 - self.b;
+    }
+}
+
+impl PartialEq for Pixel{
+    fn eq(&self, other: &Self) -> bool{
+        self.r == other.red()   && 
+        self.g == other.green() && 
+        self.b == other.blue()
+    }
+}
+
 pub struct Image {
     pub height : u8,
     pub width : u8,
--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,11 @@ mod image;
 pub extern fn dummy()->u8 {
     return 4;
 }
-
+    
 #[no_mangle]
 pub extern fn readppm(filename : String) -> image::Image{
     let mut x = Vec::new();
-    x.push(image::Pixel{r:8,g:7,b:7});
+    x.push(image::Pixel::new(8, 7, 7));
     return image::Image{
         height:8,
         width:8, 
@@ -18,30 +18,76 @@ pub extern fn readppm(filename : String) -> image::Image{
 
 #[no_mangle]
 pub extern fn write_ppm(image : image::Image){
-
 }
 
-
-
+#[no_mangle]
+pub extern fn greyscale(pixel: image::Pixel) -> image::Pixel{
+    let x = pixel.red() + pixel.green() + pixel.blue() / 3;
+    image::Pixel::new(x, x, x)
+}
+    
+    
 #[cfg(test)]
 mod tests {
     use crate::image;
+
     #[test]
-    fn pixelTest(){
-        let p = image::Pixel{r:1,g:2,b:5};
-        assert_eq!(p.r, 1);
-        assert_eq!(p.g, 2);
-        assert_eq!(p.b, 5);
+    fn pixelInit(){
+        let p = image::Pixel::new(1, 2, 5);
+        assert_eq!(p.red(), 1);
+        assert_eq!(p.green(), 2);
+        assert_eq!(p.blue(), 5);
+    }
+
+    #[test]
+    fn pixelDisplay(){
+        let p = image::Pixel::new(1, 2, 5);
+        let x = p.display();
+        assert_eq!("1,2,5", x);
+    }
+
+    #[test]
+    fn pixelInverse(){
+        let mut p = image::Pixel::new(55, 78, 10);
+        p.inverse();
+        assert_eq!(p.red(), 200);
+        assert_eq!(p.green(), 177);
+        assert_eq!(p.blue(), 245);
+    }
+
+    #[test]
+    fn pixelPartialEq(){
+        let mut p1 = image::Pixel::new(55, 78, 10);
+        let p2 = image::Pixel::new(55, 78, 10);
+        assert_eq!(p1 == p2, true);
+        assert_eq!(p1 != p2, false);
+        p1.inverse();
+        assert_eq!(p1 == p2, false);
+        assert_eq!(p1 != p2, true);
+    }
+
+    #[test]
+    fn pixelClone(){
+        let p = image::Pixel::new(10, 15, 12);
+        let mut pc = p.clone();
+        assert_eq!(p == pc, true);
+        pc.inverse();
+        assert_eq!(p == pc, false);
+    }
+
+    #[test]
+    fn pixelCopy(){
+        let p = image::Pixel::new(10, 15, 48);
+        let mut pc = p;
+        assert_eq!(p == pc, true);
+        pc.inverse();
+        assert_eq!(p != pc, true);
     }
 
     #[test]
     fn imageTest(){
         let mut x = Vec::new();
-        x.push(image::Pixel{
-            r: 8,
-            g: 10,
-            b: 15
-        });
+        x.push(image::Pixel::new(8, 10, 15));
         let image = image::Image{
             height: 5,
             width: 10,
