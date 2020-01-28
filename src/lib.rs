0d35c20c5c818e495ecdc43c2568bb7c3af653ff
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-01-28 16:05:47 +0100

    Test realized

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,3 @@
-//use std::fmt::Write;
-use std::io::Write;
-use std::io::prelude::*;
-
 mod image;
 
 #[no_mangle]
@@ -14,7 +10,7 @@ pub extern fn readppm(filename : String) -> image::Image{
     let mut x = Vec::new();
     x.push(image::Pixel{r:8,g:7,b:7});
     return image::Image{
-        height:8, 
+        height:8,
         width:8, 
         pixels: x
     };
@@ -23,4 +19,36 @@ pub extern fn readppm(filename : String) -> image::Image{
 #[no_mangle]
 pub extern fn write_ppm(image : image::Image){
 
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::image;
+    #[test]
+    fn pixelTest(){
+        let p = image::Pixel{r:1,g:2,b:5};
+        assert_eq!(p.r, 1);
+        assert_eq!(p.g, 2);
+        assert_eq!(p.b, 5);
+    }
+
+    #[test]
+    fn imageTest(){
+        let mut x = Vec::new();
+        x.push(image::Pixel{
+            r: 8,
+            g: 10,
+            b: 15
+        });
+        let image = image::Image{
+            height: 5,
+            width: 10,
+            pixels: x
+        };
+        assert_eq!(image.height, 5);
+        assert_eq!(image.width, 10);
+        assert_eq!(image.pixels.len(), 1);
+    }
 }
